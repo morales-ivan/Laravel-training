e72a451b38fe1c0cff70f79f3e73594729153ddc
Ivan <im.moralesivan@gmail.com>
2018-10-20 17:40:00 -0300

    Mejorando las views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	{{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> --}}
+	<style>
+		body {
+			background-color: #f7f9fa;
+		}
+	</style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+	<div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel sticky-top">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -48,21 +55,24 @@
                             </li>
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+								<div class="row align-items-center">
+									<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+										<img class="rounded-circle pr-1" style="height: 30px;" src="{{ Auth::user()->avatar }}">
+										{{ Auth::user()->name }} <span class="caret"></span>
+									</a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+									<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+										<a class="dropdown-item" href="{{ route('logout') }}"
+										onclick="event.preventDefault();
+										document.getElementById('logout-form').submit();">
+										{{ __('Logout') }}
+										</a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+										<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+											@csrf
+										</form>
+									</div>
+								</div>
                             </li>
                         @endguest
                     </ul>
@@ -70,12 +80,13 @@
             </div>
         </nav>
 
-        <main class="container py-4">
+        <main class="container pb-4">
 			@yield('content')
         </main>
     </div>
 
 	<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="{{ asset('js/app.js' )}}"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class='jumbotron text-center'>
+<div class='jumbotron text-center shadow'>
     <h1 class="h3">Ivan Morales</h1>
-	<nav>
+	<nav class="flex-row d-flex">
 		<ul class='nav nav-pills'>
 			@foreach ($links as $link => $text)
-				<li class='nav-item'>
-					<a class='nav-link' href="{{ $link }}">{{ $text }}</a>
+				<li class='nav-item pr-2'>
+					<a target="_blank" class='nav-link btn btn-light' href="{{ $link }}">{{ $text }}</a>
 				</li>
 			@endforeach
 		</ul>
@@ -32,14 +32,19 @@
 
 <div class="row">
 	@forelse ($photos as $photo)
-		<div class="col-6">
-			<a href="photos/{{ $photo->id }}">
-				<img class="img-thumbnail" src="{{ $photo->image }}">
-			</a>
-			<p class="card-text">
-				{{ $photo->text }}
-				<a href="photos/{{ $photo->id }}">Open</a>
-			</p>
+		<div class="col-6 mb-4 d-flex">
+		{{-- @if ($photo->id % 2) pl-3 pr-2 @else pl-2 pr-3 @endif"> --}}
+			<div class="card shadow-sm">
+				<div class="p-2">
+					<a href="photos/{{ $photo->id }}">
+						<img class="img-thumbnail" src="{{ $photo->image }}">
+					</a>
+					<p class="card-text mb-1">
+						{{ $photo->text }}
+					</p>
+					<a class="btn btn-block btn-light"href="photos/{{ $photo->id }}">Open</a>
+				</div>
+			</div>
 		</div>
 	@empty
 		<div class="col-12">
@@ -50,9 +55,11 @@
 	@endforelse
 
 	@if (count($photos))
-	<div class="mt-3 mx-auto">
-		{{ $photos->links() }}
-	</div>
+		<div class="col-12 mt-2">
+			<div class="d-flex justify-content-center">
+				{{ $photos->links() }}
+			</div>
+		</div>
 	@endif
 </div>
 @endsection
