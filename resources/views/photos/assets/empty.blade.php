342d65b805ca3543af10ef16476faef206e8febc
Ivan <im.moralesivan@gmail.com>
2018-10-30 12:59:36 -0300

    Tarjeta lista

---
+++ resources/views/photos/assets/empty.blade.php
@@ -0,0 +1,8 @@
+<div class="card shadow">
+	<div class="card-header">
+		<span class="card-title">Oops</span>
+	</div>
+	<div class="card-body">
+		<p>Parece que no hay fotos para mostrar.</p>
+	</div>
+</div>
