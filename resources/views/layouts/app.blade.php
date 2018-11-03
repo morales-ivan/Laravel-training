b7dfdcad89f23350244901473986afdba7b810eb
Ivan <im.moralesivan@gmail.com>
2018-11-03 13:18:12 -0300

    Limpiando el app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,75 +28,13 @@
 </head>
 <body>
 	<div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel sticky-top">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') . '?previous=' . Request::fullUrl() }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-								<div class="row no-gutters align-items-center">
-									<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-										<img class="rounded-circle pr-1" style="height: 30px;" src="{{ Auth::user()->avatar }}">
-										{{ Auth::user()->name }} <span class="caret"></span>
-									</a>
-
-									<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-										<a class="dropdown-item" href="/profile/{{ Auth::user()->username }}">
-											Profile
-										</a>
-										<a class="dropdown-item" href="{{ route('logout') }}"
-										onclick="event.preventDefault();
-										document.getElementById('logout-form').submit();">
-										{{ __('Logout') }}
-										</a>
-
-										<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-											@csrf
-										</form>
-									</div>
-								</div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('layouts.navbar')
 
         <main class="container py-4">
 			@yield('content')
         </main>
-
     </div>
 
-	<footer id="footer" class="footer">
-		<div class="container py-4">
-			<span class="text-muted">
-				Sitio dedicado al aprendizaje, con un footer diseñado especificamente para ello.<br>
-				© 2018 Ivan Morales
-			</span>
-		</div>
-	</footer>
+	@include('layouts.footer')
 </body>
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+	<div class="container py-4">
+		<span class="text-muted">
+			Sitio dedicado al aprendizaje, con un footer diseñado especificamente para ello.<br>
+			© 2018 Ivan Morales
+		</span>
+	</div>
+</footer>
